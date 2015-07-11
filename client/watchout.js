505020995b1d52697b0d4f17a8a9d3dc16ac61b3
johndrkurtcom <john@drkurt.com>
2015-07-11 09:54:02 -0700

    experimental d3 tests

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,10 @@
 // start slingin' some d3 here.
 
+var svg = d3.select('div').append('svg'); var asteroid = function(n){
+//asteroid.png   var asteroids = [];   for(var i=0;i<n;i++){
+asteroids.push(i);   }
+
+  var circle = svg.append('svg').attr('width', 50).attr('height', 50).append('circle').attr('cx', 25).attr('cx', 25).attr('r', 25).style('fill', 'green');
+}
+
+asteroid(5);
